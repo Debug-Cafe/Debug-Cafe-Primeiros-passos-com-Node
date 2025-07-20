d2d74991feb44a7c3375ac62338dafeae4b6b399
Alexandre Rodrigues <aleeh.rodrigues92@gmail.com>
2025-07-20 01:34:57 -0300

    Correção de códico: Lista de alunos

--- Exercicio 6/Exercicio 6 - A/calcular-media.js
+++ Exercicio 6/Exercicio 6 - A/calcular-media.js
@@ -1,3 +1,13 @@
+// Calculando Media dos Alunos
+
+const students = [
+    { name: "João", age: 20, grades: [85, 90, 78] },
+    { name: "Maria", age: 19, grades: [92, 95, 89] },
+    { name: "Pedro", age: 21, grades: [70, 65, 80] },
+    { name: "Ana", age: 18, grades: [60, 75, 68] },
+    { name: "Carlos", age: 22, grades: [90, 88, 92] }
+];
+
 const studentAverages = students.map(student => {
     const total = student.grades.reduce((sum, grade) => sum + grade, 0);
     const average = total / student.grades.length;
@@ -12,8 +22,4 @@ console.log("Médias dos alunos:");
 studentAverages.forEach(student => {
     console.log(`${student.name}: ${student.average}`);
 });
-// Exibir os resultados
-console.log("Médias dos alunos:");
-studentAverages.forEach(student => {
-    console.log(`${student.name}: ${student.average}`);
-});
+
