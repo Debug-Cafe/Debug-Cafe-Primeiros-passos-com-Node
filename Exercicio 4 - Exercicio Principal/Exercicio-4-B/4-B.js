fa822f845f6a3ec60b4d3ebca30a463223e1caba
CarolineCoutinhoHA <carolinecoutinhoh@gmail.com>
2025-07-20 16:50:03 -0300

    Resolução do desafio B do exercício 4

---
+++ Exercicio 4 - Exercicio Principal/Exercicio-4-B/4-B.js
@@ -0,0 +1,18 @@
+// Aqui eu usei apenas o "let" para alterar o "número" depois da definição inicial.
+// Também poderia ser utilizado como constante. 
+
+let numero = 4;  
+
+if (numero % 2 ==0) {
+    console.log("O número é par 🟢");
+} else {
+    console.log("O número é ímpar 🔴");
+}
+
+numero = 7;
+
+if (numero % 2 == 0) {
+    console.log("O número é par 🟢"); 
+} else {
+    console.log("O número é ímpar 🔴");
+}
